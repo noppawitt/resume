79772a56505efeaef744e55690f0400ec5e2e22e
noppawitt <noppawit.trr@gmail.com>
2018-11-08 02:00:30 +0700

    add cli component

---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import CLI from '../CLI';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CLI />
+      </div>
+    );
+  }
+}
+
+export default App;
